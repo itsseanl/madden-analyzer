7ea0eb37f85cffa884253f2833a5e4a9a344220f
root <itsseanl@gmail.com>
2020-08-25 23:25:52 +0000

    added express, downloaded folder, made demo request

---
+++ server/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+const fs = require('fs');
+
+//routes
+app.post('/:platform/:leagueID/:leagueteams', (req, res) => {
+	let body = '';
+	req.on('data', chunk => {
+		body += chunk.toString();
+	});
+	req.on('end', () => {
+		//console.log(body)
+		fs.writeFile('../public/downloaded/teamData.json', body, function(err) {
+			if(err){
+			return console.log(err);
+			}
+			else{
+			return console.log('wrote file successfully');
+			}
+		});
+	});
+});
+//start server
+app.listen(port);
+console.log('Server Started ');
