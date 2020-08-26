10173ee2402a4dad67379dd7791443d9c91eb2b7
Sean Lyons <seanlyons@Seans-MacBook-Pro.local>
2020-08-26 03:04:43 -0400

    added some pizzazzzzz

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import logo from "./logo.svg";
 import TeamData from "./components/TeamData";
-
+import Parrot from "./football_parrot.gif";
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header"></header>
+			<header className="App-header flex flex-col justify-center content-center bg-gray-800">
+				<img src={Parrot} className="p-5 w-1/4 m-auto" />
+				<h1 className="text-4xl text-white text-center p-5">
+					Madden Stats Baybeee
+				</h1>
+			</header>
 			<TeamData />
 		</div>
 	);
