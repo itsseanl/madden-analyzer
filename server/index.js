75dada23d43292f6aa6f5b52b3045af7c5b9926a
Sean Lyons <seanlyons@Seans-MacBook-Pro.local>
2020-08-26 11:22:08 -0400

    added free agent endpoint

--- server/index.js
+++ server/index.js
@@ -22,25 +22,44 @@ app.post("/:platform/:leagueID/:leagueteams", (req, res) => {
 		});
 	});
 });
-
+//team rosters
 app.post("/:platform/:leagueId/team/:teamID/roster", (req, res) => {
 	let body = "";
-	const weekNumber = req.query.weekNumber;
 	req.on("data", (chunk) => {
 		body += chunk.toString();
 	});
 	req.on("end", () => {
-		fs.writeFile(`../src/data/week${weekNumber}.json`, body, function (err) {
+		fs.writeFile(`../src/data/teamRosters.json`, body, function (err) {
 			if (err) {
 				return console.log(err);
 			} else {
 				res.sendStatus(200);
 
-				return console.log("wrote weekly stats successfully");
+				return console.log("wrote team rosters successfully");
 			}
 		});
 	});
 });
+
+//free agents
+app.post("/:username/:platform/:leagueId/freeagents/roster", (req, res) => {
+	let body = "";
+	req.on("data", (chunk) => {
+		body += chunk.toString();
+	});
+	req.on("end", () => {
+		fs.writeFile(`../src/data/freeAgents.json`, body, function (err) {
+			if (err) {
+				return console.log(err);
+			} else {
+				res.sendStatus(200);
+
+				return console.log("wrote free agents successfully");
+			}
+		});
+	});
+});
+
 //start server
 app.listen(port);
 console.log("Server Started ");
