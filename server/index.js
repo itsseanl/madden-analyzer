bceba39fe2b27f885dff98f250cb6514362ef686
Sean Lyons <seanlyons@Seans-MacBook-Pro.local>
2020-08-26 11:13:21 -0400

    changed roster endpoint

--- server/index.js
+++ server/index.js
@@ -23,27 +23,24 @@ app.post("/:platform/:leagueID/:leagueteams", (req, res) => {
 	});
 });
 
-app.post(
-	"/:platform/:leagueId/week/:weekType/:weekNumber/:dataType",
-	(req, res) => {
-		let body = "";
-		const weekNumber = req.query.weekNumber;
-		req.on("data", (chunk) => {
-			body += chunk.toString();
-		});
-		req.on("end", () => {
-			fs.writeFile(`../src/data/week${weekNumber}.json`, body, function (err) {
-				if (err) {
-					return console.log(err);
-				} else {
-					res.sendStatus(200);
+app.post("/:platform/:leagueId/roster", (req, res) => {
+	let body = "";
+	const weekNumber = req.query.weekNumber;
+	req.on("data", (chunk) => {
+		body += chunk.toString();
+	});
+	req.on("end", () => {
+		fs.writeFile(`../src/data/week${weekNumber}.json`, body, function (err) {
+			if (err) {
+				return console.log(err);
+			} else {
+				res.sendStatus(200);
 
-					return console.log("wrote weekly stats successfully");
-				}
-			});
+				return console.log("wrote weekly stats successfully");
+			}
 		});
-	}
-);
+	});
+});
 //start server
 app.listen(port);
 console.log("Server Started ");
